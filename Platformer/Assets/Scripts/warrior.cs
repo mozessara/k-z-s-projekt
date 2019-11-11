4fcf9052c3fb13a99b9767b48562a70c94d07ab2
Matyi-droid <matyaskrekity@gmail.com>
2019-11-11 15:25:27 +0100

    matyi changes

---
+++ Platformer/Assets/Scripts/warrior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarriorClass : BaseClass
+{
+    public WarriorClass(){
+        ClassName = "Warrior";
+        Health = 10;
+        Streanght = 9;
+        Intelligance = 4;
+        Speed = 4;
+        Damage = 9;
+        CanShoot = false;
+    }
+    public void Hit(){
+        
+    }
+}
