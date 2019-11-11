cbb17d9e3473cacbcf172bb2c44ffd569040f5e1
andrasPeckham <peckham0@gmail.com>
2019-11-11 15:09:40 +0100

    update

---
+++ Platformer/Assets/Scripts/BaseClass.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseClass : MonoBehaviour
+{
+    private string className;
+    private int health;
+    private int strength;
+    private int intelligence;
+    private int speed;
+    private int damage;
+    private bool canShoot;
+
+    public string ClassName{
+        get{
+            return className;
+            }
+        set{
+            className = value;
+        }
+    }
+    public int Health{
+        get{return health;}
+        set{health = value;}
+    }
+    public int Strength{
+        get{return strength;}
+        set{strength = value;}
+    }
+    public int Speed{
+        get{return speed;}
+        set{speed = value;}
+    }
+    public int Intelligence{
+        get{return intelligence;}
+        set{intelligence = value;}
+    }
+    public int Damage{
+        get{return damage;}
+        set{damage = value;}
+    }
+    public bool CanShoot{
+        get{return canShoot;}
+        set{canShoot = value;}
+    }
+}
---
+++ Platformer/Assets/Scripts/RangerClass.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangerClass : BaseClass
+{
+    public RangerClass(){
+        ClassName = "Ranger";
+        Health = 8;
+        Strength = 3;
+        Intelligence = 2;
+        Speed = 5;
+        Damage = 5;
+        CanShoot = true;
+    }
+    public void Hit(){
+        
+    }
+}
