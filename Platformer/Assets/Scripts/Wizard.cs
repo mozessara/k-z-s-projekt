42826cf9421a002c1775318308375246959db512
andrasPeckham <peckham0@gmail.com>
2019-11-11 15:34:21 +0100

    andy

--- Platformer/Assets/Scripts/Wizard.cs
+++ Platformer/Assets/Scripts/Wizard.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class Wizard : BaseClass
 {
-    public class WizardClass(){
+    public class Wizard(){
         className = "Wizard";
         Health = 5;
         Strength = 3;
         Intelligence = 8;
         Speed = 4;
         Damage = 6;
-        Canshoot = true;
+        CanShoot = true;
     }
 
     public void Hit(){}
